5d39110e824216e9504211ee8aa298ec9ce3218e
AnnaVlaBor <avborovkova@list.ru>
2022-02-07 01:25:20 +0300

    Commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,14 @@
+public class BonusMilesService {
+    public int calculate(int prise) {
+        return Miles;
+    }
+    int cost = 10_000;
+    int rubles = 20;
+    int Miles = cost / rubles;
+}
+
+
+
+    //Определите в нём метод calculate, который:
+        //Принимает на вход один параметр: cost типа int
+       // Анализируя значение переданного параметра, возвращает рассчитанное количество миль (тип - int)
